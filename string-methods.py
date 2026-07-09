3b03d6f6330058e039f338b93d52c411169d4a48
Mustafa <mustafamtu3458@gmail.com>
2026-07-10 00:58:59 +0300

    Completed String Methods

---
+++ string-methods.py
@@ -0,0 +1,23 @@
+mesaj  = "BTK Akademi , Python Kursu"
+
+# sonuc  = mesaj.upper()
+# sonuc = mesaj.lower()
+# sonuc = mesaj.title() 
+# sonuc = mesaj.capitalize()
+
+# sonuc = "abc".islower()
+
+# sonuc = mesaj.strip()
+
+#Bütün metodlar yeni bir değere taşınır
+#print(mesaj) = "BTK Akademi Python Kursu"
+
+# sonuc = mesaj.split()
+# sonuc = mesaj.split(",")
+
+# sonuc = mesaj.index("Akademi")
+# sonuc = mesaj.startswith("B")
+# sonuc = mesaj.endswith("u")
+
+sonuc = mesaj.replace("Python","C#")
+print(sonuc)
---
+++ uygulama-string-methods.py
@@ -0,0 +1,44 @@
+kursAdi = "BTK Akademi Python İle Programlama"
+website = "https://www.btkakademi.gov.tr/"
+
+# 1- ' Btk Akademi ' karakter dizisinin baş ve sondaki boşluk karakterlerini siliniz
+
+# sonuc = ' Btk Akademi'.strip()
+
+# 2- kursAdi değişkenindeki tüm karakterleri küçük harfe çeviriniz
+
+# sonuc = kursAdi.lower()
+
+# 3- website değişkeninde kaç tane "." karakteri vardır
+
+# sonuc = website.count(".")
+
+#4- website değişkeni 'https' ile mi başlıyor?
+
+# sonuc = website.startswith("https")
+
+#5- website tr ile mi bitiyor?
+
+# sonuc = website.endswith("tr")
+
+#6- kursAdi içerisindeki tüm karakterler harflerden mi oluşuyor?
+
+# sonuc = kursAdi.isalpha()
+
+#7- kursAdi değişkenindeki tüm boşlukları "-" ile değiştirin
+
+# sonuc = kursAdi.replace(" ","-")
+
+#8- kursAdi değişkenindeki Python kelimesini ReactJs ile değiştirin
+
+# sonuc = kursAdi.replace("Python","ReactJs")
+
+#9- website değişkeni 'www' içeriyor mu?
+
+# sonuc = website.find("www")
+
+#10- kursAdi değişkenini listeye çeviriniz.
+
+# sonuc = kursAdi.split()
+
+print(sonuc)
