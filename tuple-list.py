58f5ff16ccd838920157693fc153a5c6c626503d
Mustafa <mustafamtu3458@gmail.com>
2026-07-16 22:59:46 +0300

    Completed Tuple

---
+++ tuple-list.py
@@ -0,0 +1,18 @@
+my_list = [1,2,3]
+my_tuple = (1,2,3)
+
+print(type(my_list))
+print(type(my_tuple))
+
+sonuc = my_list[0] = 2
+# sonuc = my_tuple[0] = 2
+
+my_list.append(3)
+
+my_tuple2 = tuple([2,3,4])
+
+sonuc = my_tuple
+sonuc = my_list
+
+
+print(sonuc)
