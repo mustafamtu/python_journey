dcd30a090c346858652dedf987ba3e956dbd29bb
Mustafa <mustafamtu3458@gmail.com>
2026-07-18 23:43:59 +0300

    Completed Sets & Lists

---
+++ sets-list.py
@@ -0,0 +1,24 @@
+meyveler = { "elma","armut","kiraz" }
+meyveler2 = { "elma","armut","kiraz","kavun" }
+
+# sonuc = meyveler[e]
+
+# for x in meyveler:
+# print(x)
+
+# sonuc = "elma" in meyveler
+
+# meyveler.add("karpuz")
+# meyveler.update(meyveler2)
+# meyveler.remove("vişne") # raise an error
+# meyveler.discard("armut")
+
+
+#
+
+# meyveler.pop()
+meyveler.clear()
+
+sonuc = meyveler
+
+print(sonuc)
