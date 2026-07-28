c3b3f3f77bc4659ee05656008eef3591161edc23
mustafamtu <mustafamtu3458@gmail.com>
2026-07-28 03:38:32 +0300

    Learnt While Loop

---
+++ 05-Döngüler/uygulama-while-dongusu.py
@@ -0,0 +1,38 @@
+# 1- Başlangıç ve bitiş değerlerini kullanıcıdan alınız ve bu değerler arasındaki tüm çift sayıları yazdırınız.
+
+# start = int(input("Başlangıç Değeri Giriniz: "))
+# end = int(input("Bitiş Değeri Giriniz: "))
+
+# while start <=   end:
+    
+#     if(start % 2 == 0):
+#         print(start)
+#     start += 1
+
+
+# 2- (1-100) arasındaki sayıları azalan şekilde yazdırınız.
+
+
+# i = 100
+# while i >= 1:
+    
+#     print(i)
+#     i -= 1   
+
+
+# 3- Kullanıcıdan alacağınız 5 sayıyı ekranda sıralı bir şekilde yazdırın.
+
+# x = 0
+# sayilar = []
+# while x < 5:
+#     sayi = int(input("Sayı Giriniz: "))
+#     x += 1  
+#     sayilar.append(sayi)
+
+# sayilar.sort()
+# print(sayilar)
+
+
+# 4- Klavyeden girişi istenen username bilgisi için boşluk girildiği sürece tekrar username girişi isteyiniz.   
+
+# ??? 
