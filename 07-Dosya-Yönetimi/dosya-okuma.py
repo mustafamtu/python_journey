4f185c571f3a0a893be34cac4566b35ef0c06689
mustafamtu <mustafamtu3458@gmail.com>
2026-08-04 03:35:51 +0300

    Started File Management Basics.

---
+++ 07-Dosya-Yönetimi/dosya-okuma.py
@@ -0,0 +1,16 @@
+"""
+
+Dosya açmak ve oluşturmak için open() fonksiyonu kullanılır.
+
+Kullanımı:                  open(dosya_adi,dosya_erişme_modu)
+dosya_erişme_modu:          dosyayı hangi amaçla açtığımızı belirtir.
+"r" okuma modu:             belirtilen konumda dosya olmalıdır
+seek:                       cursor konumu
+
+"""
+
+f = open("log.txt", encoding = "utf-8")
+
+# print(f.read())
+# print(f.read())
+
---
+++ 07-Dosya-Yönetimi/dosya-yazma.py
@@ -0,0 +1,14 @@
+# "w": (Write) yazma modu.
+#       ** Dosyayı konumda oluşturur.
+#       ** Eğer konumda aynı dosya varsa dosyayı siler ve yeni oluşturur.
+
+with open("dosya.txt","w",encoding = "utf-8") as file:
+    file.write("Sadık Turan\n")
+    file.write("Mustafa Mutlu\n")
+
+    
+with open("dosya.txt","r",encoding = "utf-8") as file:
+    for i in file:
+        print(i, end = "")
+
+
